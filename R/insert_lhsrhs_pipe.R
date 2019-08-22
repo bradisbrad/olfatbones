f89154243887411b0e05e8b220bd5e96f808eb02
BRAD_HILL <bhill@freightwaves.com>
2019-08-22 10:45:38 -0400

    fix addin function call

--- R/insert_lhsrhs_pipe.R
+++ R/insert_lhsrhs_pipe.R
@@ -1,3 +1,10 @@
+#' Insert LHS replacement pipe
+#'
+#' @return
+#' @import magrittr
+#' @export
+#'
+#' @examples
 insert_LHSRHS_addin <- function(){
-  rstudioapi::insertText(" %<>%")
+  rstudioapi::insertText(" %<>% ")
 }
